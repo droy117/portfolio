8d63ad4cfa04e04954e20bcadf096b64121bba81
slayeh17 <debakroy7@gmail.com>
2024-05-10 02:45:47 +0530

    init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			"blueText": "rgba(24, 182, 255, 0.9)",
+ 			"orangeText": "rgba(255, 149, 40, 0.9)",
+ 			"yellowText": "rgba(221, 221, 37, 0.9)",
+			"greenText": "rgba(27, 235, 158, 0.9)",
+			"purpleText": "rgba(180, 98, 255, 0.9)",
+			"blackText": "#1a1a1a",
+			"whiteText": "#ffffff"
+		}
+	},
+	plugins: [],
+}
